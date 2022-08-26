af90ea9da2ff7f3920dba58e190ab5e42a9dee28
dxsixpc <1071667708@qq.com>
2022-08-26 18:56:27 +0800

    fix: 修复截屏在ios下第一次截屏失败的问题

--- src/html-to-image/util.ts
+++ src/html-to-image/util.ts
@@ -184,7 +184,7 @@ export function canvasToBlob(
 export function createImage(url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.onload = () => resolve(img);
+    img.onload = () => setTimeout(() => resolve(img));
     img.onerror = reject;
     img.crossOrigin = 'anonymous';
     img.decoding = 'sync';
